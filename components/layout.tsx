f6e24ac2c3dbfd4b8e99855656f2499681c170b8
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-08-22 16:13:54 -0400

    added possibility to override layout inner components

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,13 +3,35 @@ import Footer from "./footer";
 import React, { FC } from "react";
 
 import styles from "../styles/layout.module.scss";
+import {
+  ComponentProperties,
+  Components,
+  getComponents,
+} from "./utils/get-components";
 
-const Layout: FC = (properties) => (
-  <div className="layout">
-    <Header />
-    <div className={styles["content"]}>{properties.children}</div>
-    <Footer />
-  </div>
-);
+const defaultComponents = {
+  Header,
+  Footer,
+};
+type ComponentNames = keyof typeof defaultComponents;
+
+type Properties = {
+  overrides?: ComponentProperties<Components, ComponentNames>;
+};
+
+const Layout: FC<Properties> = (properties) => {
+  const {
+    Header: { props: headerProperties },
+    Footer: { props: footerProperties },
+  } = getComponents(defaultComponents, properties.overrides);
+
+  return (
+    <div className="layout">
+      <Header {...headerProperties} />
+      <div className={styles["content"]}>{properties.children}</div>
+      <Footer {...footerProperties} />
+    </div>
+  );
+};
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,36 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+import styles from "../styles/home.module.scss";
 import Layout from "../components/layout";
 import { isAuthenticated } from "../src/utils/auth";
 
 export const Home: FC = (): JSX.Element => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const router = useRouter();
 
   useEffect(() => {
     if (!isAuthenticated()) {
       void router.push("/login");
     }
-  });
+
+    setIsLoading(false);
+  }, [isLoading, router]);
 
   return (
     <>
-      <Head>
-        <title>Menmos</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>LOGGED IN</Layout>
+      {!isLoading && (
+        <>
+          <Head>
+            <title>Menmos</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Layout>
+            <div className={styles["container"]}></div>
+          </Layout>
+        </>
+      )}
     </>
   );
 };
